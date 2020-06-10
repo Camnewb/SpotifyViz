4a474d704c668aaeab0bdab787a7be8bee0c4df1
morabrandoi <morabrandoi@gmail.com>
2020-06-10 14:46:23 -0400

    organized Data and outlined graph formation

--- core_model/graph_net.py
+++ core_model/graph_net.py
@@ -1,3 +1,26 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import csv
 
+CONNECTIONS = 3
+EDGE_THRESHOLD = 0
+
+# initiliazing file reader and graph
+filePath = './csv/test.csv'
+inFile = open(filePath)
+reader = csv.DictReader(inFile)
+G = nx.Graph()
+
+# get new artist
+for _ in range(60):
+    print(reader.next)
+# Least squares it against all other nodes
+# Pick CONNECTIONS nodes with lowest difference squared score (maybe consider using a threshold)
+# Add node and add edges to create graph
+    # Go through each feature, find the difference squared and assign that feature as an edge if its under the threshold
+    # 
+
+
+
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
