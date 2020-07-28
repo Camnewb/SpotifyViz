e5373c27f12aaebb8260dd42468eb205d9934710
Brando Mora <morabrandoi@gmail.com>
2020-07-27 23:30:23 -0700

    added code to push raw songs to firebase

--- backend/get_recs/functions/src/index.ts
+++ backend/get_recs/functions/src/index.ts
@@ -4,33 +4,42 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 const db = admin.firestore()
 
-// TODO: Implement JSON making of nodes
-
 export const getLocalGraph = functions.https.onRequest(async (request, response) => {
     try{
         // Pulling in query variables
         const songName : string = request.query.song as string;
         const queryLimit : number = parseInt(request.query.limit as string);
 
+        // initializing objects for making json Object
+        const nodeList : Array<Object> = new Array<Object>();
+        // const edgeList : Array<Object> = new Array<Object>();
+
+        // making sure song name was really passed in
         if (songName === undefined){
             throw Error("Song name was not provided");
         }
-
+        
         console.log(`Song name ${songName}, queryLimit: ${queryLimit}`);
-    
+        
+        // retrieving firestore doc for passed in song name
         const query = db.collection("songs").where('name', '==', songName).limit(1);
         const docsWithSongName = await query.get();
         
+        // making sure document with that name really exists. If not throw error.
         if (docsWithSongName.empty){
-            console.log("the document was empty")
+            console.error("the document was empty")
             throw Error("That song does not exist in our database");
         }
         const songDoc = docsWithSongName.docs[0];
+
+        // Pushing that node data into nodeList
+        nodeList.push( songDoc.data() ); 
         
-        
+        // putting initial node in format of rest of queue 
         const songNameIDCombo : string = songDoc.get("name") + "|" + songDoc.get("id");
         console.log(`songDoc combo: ${songNameIDCombo}`);
-        let traversal : string = "";
+
+        // initializing traversal variables
         const queue : Array<string> = [];
         const visited : Set<string> = new Set();
         let popped : number = 0;
@@ -42,15 +51,17 @@ export const getLocalGraph = functions.https.onRequest(async (request, response)
         while ((popped <= queryLimit) && queue.length !== 0){
             const curSong : string = queue.shift() as string;
             popped += 1
-            const [curSongName, curSongSpotifyID] : Array<string> = getSpotifyID(curSong);
+
+            const curSongSpotifyID : string = getSpotifyID(curSong)[1];
 
             if (curSongSpotifyID === undefined){
                 continue;
             }
-
-            traversal += `name: ${curSongName}, spotID: ${curSongSpotifyID}\n`
             
-            const doc = await getRelated(curSongSpotifyID);
+            const doc = await getDoc(curSongSpotifyID);
+
+            nodeList.push( doc.data() );
+
             const docConnectedTo : Array<string> = doc.get("connected_to");
             docConnectedTo.forEach( (songCombo : string ) => {
                 if (!visited.has(songCombo)){
@@ -58,10 +69,9 @@ export const getLocalGraph = functions.https.onRequest(async (request, response)
                     queue.push(songCombo);
                 }
             });
-
         }
         
-        response.send(traversal);
+        response.send( "test " + JSON.stringify( {"nodes" : nodeList} ) );
     }
     catch (err){
         response.status(500).send(err);
@@ -69,7 +79,8 @@ export const getLocalGraph = functions.https.onRequest(async (request, response)
     }
 });
 
-async function getRelated(spotifyID : string) {
+// 
+async function getDoc(spotifyID : string) {
     console.log(`trying to find song by spotifyID: ${spotifyID}`)
     const query = db
             .collection("songs")
