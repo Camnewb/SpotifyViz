f2687974819a7eb390dd401573e7b0b82eb1e836
Brando Mora <morabrandoi@gmail.com>
2020-07-28 00:06:59 -0700

    refactored firebase functions

--- backend/firebaseFunctions/functions/src/index.ts
+++ backend/firebaseFunctions/functions/src/index.ts
@@ -4,6 +4,14 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 const db = admin.firestore()
 
+export const getGraphFromRawSongs = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+
+
+
 export const getLocalGraph = functions.https.onRequest(async (request, response) => {
     try{
         // Pulling in query variables
