20e5fe63e038af5541c69f3b000e7ca028333154
Cameron Newborn <camnewborn@gmail.com>
2020-07-27 23:36:19 -0400

    Graph updates
    Updates to the look and feel of the graph
    New example data set because the last one was cumbersome
    Minor css color change
    Hopefully the next commit will have mouseover functionality

--- frontend/d3-graph.js
+++ frontend/d3-graph.js
@@ -1,10 +1,10 @@
 //This code was stolen from https://bl.ocks.org/jodyphelan/5dc989637045a0f48418101423378fbd
-var radius = 10;
+var radius = 5;
 
 var defaultNodeCol = "white",
     highlightCol = "yellow";
 
-var height = window.innerHeight;
+var height = window.innerHeight - 100;
 var graphWidth =  window.innerWidth;
 
 var graphCanvas = d3.select('#graph-window').append('canvas')
@@ -30,108 +30,111 @@ var simulation = d3.forceSimulation()
 
 var transform = d3.zoomIdentity;
 
-var dataFile = 'data.json';
+updateFile();
 
-d3.json(dataFile, function(error,data){
-  console.log(data)
-  initGraph(data)
-  function initGraph(tempData){
+function updateFile() {
 
+  var dataFile = 'data.json';
 
-    function zoomed() {
-      console.log("zooming")
-      transform = d3.event.transform;
-      simulationUpdate();
-    }
+  d3.json(dataFile, function(error,data){
+    console.log(data);
+    initGraph(data);
+    function initGraph(jsonData) {
+      function zoomed() {
+        console.log("zooming");
+        transform = d3.event.transform;
+        tick();
+      }
 
-    //Add drag and zoom functions to the canvas
-    d3.select(graphCanvas)
-        .call(d3.drag().subject(dragsubject).on("start", dragstarted).on("drag", dragged).on("end",dragended))
-        .call(d3.zoom().scaleExtent([1/2, 10]).on("zoom", zoomed))
+      //Add drag and zoom functions to the canvas
+      d3.select(graphCanvas)
+          .call(d3.drag().subject(dragsubject)
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end",dragended))
+          .call(d3.zoom()
+          .scaleExtent([1/2, 10])
+          .on("zoom", zoomed)); 
 
+      //Drag function
+      function dragsubject() {
+        var i,
+        x = transform.invertX(d3.event.x),
+        y = transform.invertY(d3.event.y),
+        dx, dy;
+        for (i = jsonData.nodes.length - 1; i >= 0; --i) {
+          node = jsonData.nodes[i];
+          dx = x - node.x;
+          dy = y - node.y;
 
-  //Drag function
-  function dragsubject() {
-    var i,
-    x = transform.invertX(d3.event.x),
-    y = transform.invertY(d3.event.y),
-    dx,
-    dy;
-    for (i = tempData.nodes.length - 1; i >= 0; --i) {
-      node = tempData.nodes[i];
-      dx = x - node.x;
-      dy = y - node.y;
+          if (dx * dx + dy * dy < radius * radius) {
+            node.x =  transform.applyX(node.x);
+            node.y = transform.applyY(node.y);
+            return node;
+          }
+        }
+      }
 
-      if (dx * dx + dy * dy < radius * radius) {
+      //Update graph while node is being dragged
+      function dragstarted() {
+        if (!d3.event.active) simulation.alphaTarget(0.7).restart();
+        d3.event.subject.fx = transform.invertX(d3.event.x);
+        d3.event.subject.fy = transform.invertY(d3.event.y);
+      }
 
-        node.x =  transform.applyX(node.x);
-        node.y = transform.applyY(node.y);
+      function dragged() {
+        d3.event.subject.fx = transform.invertX(d3.event.x);
+        d3.event.subject.fy = transform.invertY(d3.event.y);
+        console.log("dragged " + d3.event.subject.id + " to x= " + d3.event.x + ", y: " + d3.event.y)
+      }
 
-        return node;
+      function dragended() {
+        if (!d3.event.active) simulation.alphaTarget(0.01);
+        d3.event.subject.fx = null;
+        d3.event.subject.fy = null;
+      }
+
+      simulation.nodes(jsonData.nodes)
+                .on("tick", tick);
+
+      simulation.force("link")
+                .links(jsonData.edges);
+
+      //Draws the nodes and edges using canvas every update
+      function tick() {
+        context.save();
+        context.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
+        context.translate(transform.x, transform.y);
+        context.scale(transform.k, transform.k);
+
+        jsonData.edges.forEach(function(d) {
+          //Draw the edges between nodes
+          context.beginPath();
+          context.moveTo(d.source.x, d.source.y);
+          context.lineTo(d.target.x, d.target.y);
+          context.strokeStyle = defaultNodeCol;
+          context.lineWidth = 2;
+          context.globalAlpha = 0.75;
+          context.stroke();
+        });
+
+        // Draw the nodes
+        jsonData.nodes.forEach(function(d, i) {
+          context.beginPath();
+          context.arc(d.x, d.y, radius * 1.2, 0, 2 * Math.PI, true);
+          context.fillStyle = "white";
+          context.fill();
+          context.closePath();
+          context.beginPath();
+          context.arc(d.x, d.y, radius, 0, 2 * Math.PI, true);
+          context.fillStyle = "#3ca0f3";
+          context.globalAlpha = 1;
+          context.fill();
+        });
+
+        context.restore();
+  //        transform = d3.zoomIdentity;
       }
     }
-  }
-
-
-  function dragstarted() {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d3.event.subject.fx = transform.invertX(d3.event.x);
-    d3.event.subject.fy = transform.invertY(d3.event.y);
-  }
-
-  function dragged() {
-    d3.event.subject.fx = transform.invertX(d3.event.x);
-    d3.event.subject.fy = transform.invertY(d3.event.y);
-    console.log("dragged " + d3.event.subject.id + " to x= " + d3.event.x + ", y: " + d3.event.y)
-  }
-
-  function dragended() {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d3.event.subject.fx = null;
-    d3.event.subject.fy = null;
-  }
-
-    simulation.nodes(tempData.nodes)
-              .on("tick",simulationUpdate);
-
-    simulation.force("link")
-              .links(tempData.edges);
-
-    //Draws the nodes and edges using canvas every update
-    function simulationUpdate() {
-      context.save();
-
-      context.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
-      context.translate(transform.x, transform.y);
-      context.scale(transform.k, transform.k);
-
-      tempData.edges.forEach(function(d) {
-        //Draw the edges between nodes
-        context.beginPath();
-        context.moveTo(d.source.x, d.source.y);
-        context.lineTo(d.target.x, d.target.y);
-        context.strokeStyle = defaultNodeCol;
-        context.lineWidth = 3;
-        context.globalAlpha = 0.75;
-        context.stroke();
-      });
-
-      // Draw the nodes
-      tempData.nodes.forEach(function(d, i) {
-        context.beginPath();
-        context.arc(d.x, d.y, radius * 1.2, 0, 2 * Math.PI, true);
-        context.fillStyle = "blue";
-        context.fill();
-        context.closePath();
-        context.beginPath();
-        context.arc(d.x, d.y, radius, 0, 2 * Math.PI, true);
-        context.fillStyle = d.col ? highlightCol : defaultNodeCol;
-        context.globalAlpha = 1;
-        context.fill();
-      });
-
-      context.restore();
-//        transform = d3.zoomIdentity;
-    }
-  }
-})
+  })
+}
