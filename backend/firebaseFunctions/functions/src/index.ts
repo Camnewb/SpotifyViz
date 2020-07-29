3830ac9dd825651615f192abc0d0d9848f411179
Brando Mora <morabrandoi@gmail.com>
2020-07-29 00:08:23 -0700

    implemented basic structure of query and resposne

--- backend/firebaseFunctions/functions/src/index.ts
+++ backend/firebaseFunctions/functions/src/index.ts
@@ -4,111 +4,78 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 const db = admin.firestore()
 
-export const getGraphFromRawSongs = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-
-
-
-export const getLocalGraph = functions.https.onRequest(async (request, response) => {
-    try{
-        // Pulling in query variables
-        const songName : string = request.query.song as string;
-        const queryLimit : number = parseInt(request.query.limit as string);
-
-        // initializing objects for making json Object
-        const nodeList : Array<Object> = new Array<Object>();
-        // const edgeList : Array<Object> = new Array<Object>();
-
-        // making sure song name was really passed in
-        if (songName === undefined){
-            throw Error("Song name was not provided");
-        }
-        
-        console.log(`Song name ${songName}, queryLimit: ${queryLimit}`);
-        
-        // retrieving firestore doc for passed in song name
-        const query = db.collection("songs").where('name', '==', songName).limit(1);
-        const docsWithSongName = await query.get();
-        
-        // making sure document with that name really exists. If not throw error.
-        if (docsWithSongName.empty){
-            console.error("the document was empty")
-            throw Error("That song does not exist in our database");
-        }
-        const songDoc = docsWithSongName.docs[0];
-
-        // Pushing that node data into nodeList
-        nodeList.push( songDoc.data() ); 
-        
-        // putting initial node in format of rest of queue 
-        const songNameIDCombo : string = songDoc.get("name") + "|" + songDoc.get("id");
-        console.log(`songDoc combo: ${songNameIDCombo}`);
-
-        // initializing traversal variables
-        const queue : Array<string> = [];
-        const visited : Set<string> = new Set();
-        let popped : number = 0;
-
-        // Initialzing queue and set with connected from original song
-        queue.push(songNameIDCombo);
-        visited.add(songNameIDCombo);
-        
-        while ((popped <= queryLimit) && queue.length !== 0){
-            const curSong : string = queue.shift() as string;
-            popped += 1
-
-            const curSongSpotifyID : string = getSpotifyID(curSong)[1];
-
-            if (curSongSpotifyID === undefined){
-                continue;
-            }
-            
-            const doc = await getDoc(curSongSpotifyID);
-
-            nodeList.push( doc.data() );
-
-            const docConnectedTo : Array<string> = doc.get("connected_to");
-            docConnectedTo.forEach( (songCombo : string ) => {
-                if (!visited.has(songCombo)){
-                    visited.add(songCombo);
-                    queue.push(songCombo);
-                }
-            });
-        }
-        
-        response.send( "test " + JSON.stringify( {"nodes" : nodeList} ) );
-    }
-    catch (err){
-        response.status(500).send(err);
-        
-    }
-});
-
-// 
-async function getDoc(spotifyID : string) {
-    console.log(`trying to find song by spotifyID: ${spotifyID}`)
-    const query = db
-            .collection("songs")
-            .where('id', '==', spotifyID)
-            .limit(1);
-        
-    const results = await query.get();
-    if (results.empty){
-        return Promise.reject("Could not find any songs by that ID")
-    }
+class Edge {
     
-    return results.docs[0];
+    source : string = "";
+    target : string = "";
+    constructor(source : string, target : string){
+        this.source = source;
+        this.target = target;
+    }
 }
 
-// Returns the [name, spotifyID]
-function getSpotifyID(combo : string) : Array<string> {
-    console.log(`about to split ${combo}`)
-    const comboList : Array<string> = combo.split("|");
-    const spotifyID : string = comboList.pop() as string;
-    const name : string = comboList.join("");
-    console.log(`split into ${spotifyID} and ${name}`)
-    return [name, spotifyID];
+export const getGraphFromRawSongs = functions.https.onRequest(async (request, response) => {
+    try {
+        // query paramters
+        const songName : string = request.query.song as string;
+        const limit : number = parseInt(request.query.limit as string);
+        // const connections: number = parseInt(request.query.connections as string);
+
+        console.log(`songName: ${songName}, queryLimit: ${limit}`);        
+        
+        if (songName === undefined){ throw Error("Song name was not provided"); }
+
+        // Getting song that was passed in query
+        const nameSongQuery = db.collection("raw_songs").where('name', '==', songName).limit(1);
+        const docsWithSongName = await nameSongQuery.get();
+        if (docsWithSongName.empty){
+            console.error("the document was empty")
+            throw Error("Could not find song with that name"); }
+        const songDoc = docsWithSongName.docs[0];
+        
+        // getting songs naively similar to that
+        const similarQuery = db.collection('raw_songs')
+            .where('year', '==', songDoc.get('year') )
+            .where('energy', '>=', songDoc.get('energy') - 0.05)
+            .where('energy', '<=', songDoc.get('energy') + 0.05)
+            .limit(50);
+        const docsSimilar = await similarQuery.get();
+        
+        
+        const docList : Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>> = new Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>();
+        const nodeList : Array<Object> = new Array<Object>();
+
+        docsSimilar.forEach(document => {
+            docList.push( document )
+            nodeList.push( document.data() )
+        });
+        
+        const edgeList : Array<Edge> = constructEdgeList(docList);
+
+        response.send( JSON.stringify( {'nodes' : nodeList, 'edges' : edgeList}) );
+    }
+    catch (err){
+        console.error(err);
+        response.status(500).send(err);
+    }
+});
+
+function constructEdgeList(nodeList : Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>) : Array<Edge> {
+    const edgeList : Array<Edge> = new Array<Edge>();
+    nodeList.forEach(doc => {
+        nodeList.sort( function (doc1 : Object, doc2: Object) : number {
+            return compareSongs(doc, doc1) - compareSongs(doc, doc2);    
+        });
+        (nodeList.slice(1, 3)).forEach(simDoc => {
+            const edge = new Edge( doc["id"], simDoc["id"] );
+            edgeList.push(edge);
+        });
+    });
+    
+    return edgeList;
+}
+
+// TODO: implement actual logic
+function compareSongs(song1 : Object, song2: Object) : number{
+    return 0;
 }
