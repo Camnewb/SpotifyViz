0ede11d6c925c4d659832c48881a3a126c15744c
Cameron Newborn <camnewborn@gmail.com>
2020-07-30 14:46:51 -0400

    Cleaning up the code
    Adding some more documentation and removing unneeded code

--- frontend/javascript/d3-graph.js
+++ frontend/javascript/d3-graph.js
@@ -1,7 +1,7 @@
-//This code is based off of https://bl.ocks.org/jodyphelan/5dc989637045a0f48418101423378fbd
+//This code was based off of https://bl.ocks.org/jodyphelan/5dc989637045a0f48418101423378fbd
 
 //========================
-//   Defining variables
+// Initializing variables
 //========================
 
 var radius = 5;//Radius of the nodes
@@ -32,59 +32,27 @@ var transform = d3.zoomIdentity;//For zooming functionality
 //      HTTP Request
 //========================
 //Send an HTTP get request to the server to retrieve the JSON data
-//Synchronus freezes the site while waiting for the request to finish
-//Asynchronus does the server request in the background, initializing the graph once the request is ready
 
-// function getDataSynchronous(url) {
-//   //Get request for the json data
-//   //TODO This needs to be turned into an asynchronus request
-//   console.log("Sending request...");
-//   var Httpreq = new XMLHttpRequest();
-//   Httpreq.open("GET", url, false);
-//   Httpreq.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-//   Httpreq.send(null);
-//   console.log("Data recieved...");
-//   console.log("JSON: " + Httpreq.responseText);
-//   return Httpreq.responseText; 
-// }
-
+//getDataAsynchronus does the server request in the background, initializing the graph once the request is ready
 function getDataAsynchronous(url) {
-  // console.log("Sending request...");
-  // var xhr = new XMLHttpRequest();
-  // xhr.open("GET", url, true);
-  // xhr.onload = function (e) {
-  //   if (xhr.readyState === 4) {
-  //     if (xhr.status === 200) {
-  //       console.log(xhr.responseText);
-  //       return xhr.responseText;
-  //     } else {
-  //       console.log(xhr.statusText);
-  //     }
-  //   }
-  // };
-  // xhr.onerror = function (e) {
-  //   console.log(xhr.statusText);
-  // };
-  // xhr.send(null);
-  // console.log("Data recieved...");
-  // console.log("JSON: " + xhr.responseText);
   console.log("Sending request...");
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function (e) {
+    //On request state change, check if the request is ready
     if (this.readyState == 4 && this.status == 200) {
+      //If it's ready, parse the JSON and call initGraph()
       console.log("Data Recieved: " + xhr.responseText);
       var jsonData = JSON.parse(xhr.responseText);
-      initgraph(jsonData);
+      initgraph(jsonData);//Initialize the graph
     }
   };
   xhr.open("GET", url, true);
   xhr.send();
   console.log("Request sent. Waiting for response...");
-  // console.log("JSON: " + xhr.responseText);
   
 }
 
-//query("American Idiot")
+//Processes a query from search()
 function query(song) {
   console.log("Starting query for " + song);
   song = song.replace(/\ /g, "%20");//Replace spaces with %20
@@ -99,22 +67,7 @@ function query(song) {
 
 function initgraph(jsonData) {
 
-  if (jsonData == undefined) {
-    console.log("JSON data is empty.");
-    return;
-  } else {
-    console.log("JSON data parsed: " + jsonData)
-  }
-
-  //var dataFile = "reformatted_sample_graph.json";
-
-  // d3.json(dataFile, function(error,data){
-  //   console.log(data);
-  function zoomed() {
-    //console.log("zooming");
-    transform = d3.event.transform;
-    tick();
-  }
+  console.log("Drawing Graph...");
 
   //========================
   // Zoom and Drag Functions
@@ -148,7 +101,7 @@ function initgraph(jsonData) {
     }
   }
 
-  //Drag functions
+  //Drag functions. This is boilerplate code
   function dragstarted() {
     if (!d3.event.active) simulation.alphaTarget(0.7).restart();
     d3.event.subject.fx = transform.invertX(d3.event.x);
@@ -158,7 +111,6 @@ function initgraph(jsonData) {
   function dragged() {
     d3.event.subject.fx = transform.invertX(d3.event.x);
     d3.event.subject.fy = transform.invertY(d3.event.y);
-    //console.log("dragged " + d3.event.subject.id + " to x= " + d3.event.x + ", y: " + d3.event.y)
   }
 
   function dragended() {
@@ -167,6 +119,11 @@ function initgraph(jsonData) {
     d3.event.subject.fy = null;
   }
 
+  function zoomed() {
+    transform = d3.event.transform;
+    tick();
+  }
+  
   //Adding nodes and links to the simulation
   //The tick function that draws nodes will execute every tick
   simulation.nodes(jsonData.nodes).on("tick", tick);
@@ -176,10 +133,10 @@ function initgraph(jsonData) {
   //Stolen from this stackoverflow thread: https://stackoverflow.com/questions/38271595/tooltips-with-canvas-networks
   var closeNode;
   d3.select("canvas").on("mousemove", function(){
-    var mouse = transform.invert(d3.mouse(this));
-    //console.log("Mouse is at x = " + mouse[0] + ", y = " + mouse[1])
+    var mouse = transform.invert(d3.mouse(this));//Coordinates of the mouse must be inverted
+    //Finds the node closest to the mouse coordinates. Returns undefined if the mouse is not within the node's radius
     closeNode = simulation.find(mouse[0], mouse[1], radius);
-    tick();
+    tick();//Update the graph to show the highlighted node
   })
   //On mouse click of a node, open a google search for the song and artist
   d3.select("canvas").on("click", function(){
@@ -192,16 +149,17 @@ function initgraph(jsonData) {
   //========================
   //  Drawing the Graph
   //========================
-  //Draws the nodes and edges of the graph every tick
+  //Draws the nodes and edges of the graph each tick
   function tick() {
-    //Start up the next drawing tick
+    //Start up the current drawing tick
     context.save();
-    context.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
-    context.translate(transform.x, transform.y);
-    context.scale(transform.k, transform.k);
+    context.clearRect(0, 0, graphCanvas.width, graphCanvas.height);//Clear the canvas
+    context.translate(transform.x, transform.y);//Pan the camera to the last state
+    context.scale(transform.k, transform.k);//Zoom the camera to the last state
 
-    //Drawing the linke between nodes
+    //Draw the linke between nodes
     jsonData.edges.forEach(function(d) {
+      //White line
       context.beginPath();
       context.moveTo(d.source.x, d.source.y);
       context.lineTo(d.target.x, d.target.y);
@@ -211,7 +169,7 @@ function initgraph(jsonData) {
       context.stroke();
     });
 
-    // Draw the nodes
+    //Draw the nodes
     jsonData.nodes.forEach(function(d) {
       //White border
       context.beginPath();
@@ -227,15 +185,14 @@ function initgraph(jsonData) {
       context.fill();
     });
 
-    //If the mouse is over a node
+    //If the mouse is over a node, same as (closeNode != undefined)
     if (closeNode) {
-      //console.log("Node x = " + closeNode.x + ", y = " + closeNode.y)
       //Fill with red
       context.beginPath();
       context.arc(closeNode.x, closeNode.y, radius, 0, 2 * Math.PI, true);
       context.fillStyle = "#9e2c2c";
       context.fill();
-      //Display the song's name
+      //Display the song's name over the node
       context.fillStyle = "#ffffff"
       context.font = "12px sans-serif";
       context.fillText(closeNode.name, closeNode.x - closeNode.name.length * 6 / 2, closeNode.y - 12);
@@ -245,5 +202,4 @@ function initgraph(jsonData) {
     //End drawing
     context.restore();
   }
-//});
 }
--- frontend/javascript/filter-menu.js
+++ frontend/javascript/filter-menu.js
@@ -10,12 +10,8 @@ for (var i = 0; i < carets.length; i++) {
  carets[i].dispatchEvent(new Event("click"));//Open the caret
 }
 
-//Set the current date for the date select and year input box
-// var d = new Date();
-// document.getElementById("release-date-select").value = getDateDashFormat(d);
-// document.getElementById("year-input").value = d.getFullYear();
-
 //Set all checkbox labels to change color when the checkbox is checked
+//Uses a stack of all the colors and pops each off to give them to a checkbox label when checked
 var checkboxes = document.getElementsByClassName("form-check-input mr-2");//Find all carets
 var colors = ["label-active-1", "label-active-2", "label-active-3", 
 "label-active-4", "label-active-5", "label-active-6", 
@@ -26,32 +22,36 @@ for (var i = 0; i < checkboxes.length; i++) {
     //checkboxLabel refers to the id of the checkbox label
     var checkboxLabel = this.id + "-label";
     if (document.getElementById(this.id).checked) {
+      //Add the color to the checkbox label and remove the label from the list of active labels
       document.getElementById(checkboxLabel).className = colors.shift(checkboxLabel);
     } else {
-      colors.unshift(document.getElementById(checkboxLabel).className);//Remove the label from the list of active labels
-      document.getElementById(checkboxLabel).className = "";
+      colors.unshift(document.getElementById(checkboxLabel).className);//Add the label back to the list of active labels
+      document.getElementById(checkboxLabel).className = "";//Remove the color from the checkbox label
     }
-    //colors.sort();
   }, );
 }
 
-//====================================================================
+//==========================
+//      Functions
+//==========================
+
 //Check if enter key is pressed, then query a song
 function search(value) {
   if (event.key === 'Enter') {
-      event.preventDefault();
+      event.preventDefault();//Stops the page from reloading
       query(value);
   }
 }
 
-//Edits the innerText of the document element with an id of target
+//Utilty function - edits the innerText of the document element with an id of target
 function editInnerText(value, target) {
   document.getElementById(target).innerText = value;
 }
 
 //Takes a range value (0-100) and returns an easily readable quartic power of the value
 function quarticSliderDisplay(sliderValue) {
-  var value = Math.pow(sliderValue, 4) * 10;
+  var value = Math.pow(sliderValue, 4) * 10;//This is the new value to be represented
+  //Add commas or prefices to make it readable
   var display = value.toString();
   if (value < 10000000) {
     if (value > 999) display = insertAt(display, ",", display.length - 3);
@@ -64,14 +64,7 @@ function quarticSliderDisplay(sliderValue) {
   return display;
 }
 
+//Utility function - inserts a string at the desired index
 function insertAt(str, insert, index) {
   return str.slice(0, index) + insert + str.slice(index, str.length);
 }
-
-// function getDateDashFormat(d) {
-//   dateString = "";
-//   dateString = dateString + d.getFullYear() + "-";
-//   dateString = dateString + ((d.getMonth() + 1).toString().length < 2 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1)) + "-";
-//   dateString = dateString + (d.getDate().toString().length < 2 ?  "0" + d.getDate() : d.getDate());
-//   return dateString;
-// }
