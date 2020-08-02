ba2bbdbce788d08f782c7f2c49588d1b33646090
Samuel Marrero <helicopter1339@gmail.com>
2020-08-02 00:05:05 -0400

    Update searches.js
    BIG: added depth-first search
    ensured compatibility in getNeighbors()
    
    made small formatting changes for uniformity

--- frontend/javascript/searches.js
+++ frontend/javascript/searches.js
@@ -1,68 +1,88 @@
-// From JSON file's nodes, get song data by name
+//From JSON file's nodes, get song data by name
 function getSongByName(songName) {
-    let data = getData()
-
+    let data = getData();
     for (node of data.nodes) {
         if (node.name === songName) {
-            return node
+            return node;
         }
     }
 }
-// From JSON file's nodes, get song data by ID
+//From JSON file's nodes, get song data by ID
 function getSongByID(songID) {
-    let data = getData()
+    let data = getData();
     for (node of data.nodes) {
         if (node.id === songID)
-            return node
+            return node;
     }
 }
 
-// From JSON file's edge list, get neighbors by a song's ID
+//From JSON file's edge list, get neighbors by a song's ID
 function getNeighbors(songID) {
-    let data = getData()
-    // from the song's JSON data, 
-    // pull the edge list to see what neighbors the song has.
-    var neighbors = []
-
+    let data = getData();
+    //from the song's JSON data, 
+    //pull the edge list to see what neighbors the song has.
+    var neighbors = [];
     
     data.edges.forEach(function(edge) {
-        if (edge.source === songID)
-            neighbors.push(getSongByID(edge.target))
-        else if (edge.target === songID) // Bi-directional
-        neighbors.push(getSongByID(edge.source))
-
+        if (edge.source.id === songID)
+            neighbors.push(getSongByID(edge.target.id));
+        else if (edge.target.id === songID) //Bi-directional
+        neighbors.push(getSongByID(edge.source.id));
     })
-
     return neighbors
 }
 
-function breadthFS(parent, numResults) {
-    var returnList = []
+//Depth-First Search
+function depthFS(parent, numResults) {
+    var returnList = [];
+    var stack = [];
+    stack.unshift(parent);
+    var visited = [];
+    visited.push(parent);
 
-    var q = []
-    q.unshift(parent) // FIRST IN
-    
-    var visited = []
-    visited.push(parent)
-
-    while (q.length > 0) {
+    while (stack.length > 0) {
         if (returnList.length === numResults+1)
-            break
+            break;
         
-        var cur = q.shift()   
-        returnList.push(cur)
+        var cur = stack.pop();
+        returnList.push(cur);
         
         getNeighbors(cur.id).forEach(function(neighbor) {
             if (!visited.includes(neighbor)) {
-                visited.push(neighbor)
-                q.unshift(neighbor)
+                visited.push(neighbor);
+                stack.unshift(neighbor);
             }
         })
     }
-    return returnList
+    return returnList;
 }
 
-// Generate edges needed for D3
+//Breadth-First Search
+function breadthFS(parent, numResults) {
+    var returnList = [];
+    var q = [];
+    q.unshift(parent);
+    var visited = [];
+    visited.push(parent);
+
+    while (q.length > 0) {
+        if (returnList.length === numResults+1)
+            break;
+        
+        var cur = q.shift();
+        returnList.push(cur);
+        
+        getNeighbors(cur.id).forEach(function(neighbor) {
+            if (!visited.includes(neighbor)) {
+                visited.push(neighbor);
+                q.unshift(neighbor);
+            }
+        })
+    }
+    return returnList;
+}
+
+//Generate edges needed for D3 (USELESS NOW)
 function generateEdges(searchResult) {
     class Edge {
         constructor(source, target) {
@@ -70,7 +90,7 @@ function generateEdges(searchResult) {
             this.target = target;
         }
     }
-    edges = []
+    edges = [];
     
     function getNextSong(songID) {
         let index = searchResult.indexOf(getSongByID(songID));
@@ -82,9 +102,9 @@ function generateEdges(searchResult) {
     for (song of searchResult) {
         let nextSong = getNextSong(song.id)
         if (nextSong != null) {
-                let newEdge = new Edge(song, nextSong)
-                edges.push(newEdge)
+                let newEdge = new Edge(song, nextSong);
+                edges.push(newEdge);
             }
         }
-    return edges
+    return edges;
 }
